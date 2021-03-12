98b6ffbe8843f0ed2f1af2a88636f159c1949bcc
jltimm <jacobltimm@gmail.com>
2021-03-11 20:37:40 -0600

    Switching to 2d array

--- src/components/Graph/Graph.js
+++ src/components/Graph/Graph.js
@@ -1,12 +1,12 @@
-import React, {useRef, useCallback} from "react";
+import React, {useState, useCallback, useMemo} from "react";
 import data from "../../data/data.json"
-import ForceGraph3D from 'react-force-graph-3d';
+import ForceGraph2D from 'react-force-graph-2d';
 
 const createNodesAndEdges = () => {
   var nodes = []
   var links = []
   data.all_artists.forEach(item => {
-    nodes.push({ id: item.id, name: item.name, val: item.popularity / 10.0 })
+    nodes.push({ id: item.id, name: item.name, val: 1 })
     item.recommended.forEach(recommended => {
       links.push({ source: item.id, target: recommended })
     })
@@ -17,28 +17,82 @@ const createNodesAndEdges = () => {
   }
 }
 
-export default function Graph(props) {
-  const fgRef = useRef();
+export default function Graph() {
+  const NODE_R = 8;
+  const data = useMemo(() => {
+    const gData = createNodesAndEdges();
 
-  const handleClick = useCallback(node => {
-    // Aim at node from outside it
-    const distance = 40;
-    const distRatio = 1 + distance/Math.hypot(node.x, node.y, node.z);
+    // cross-link node objects
+    gData.links.forEach(link => {
+      const a = gData.nodes.filter(it => it.id === link.source)[0];
+      const b = gData.nodes.filter(it => it.id === link.target)[0];
+      !a.neighbors && (a.neighbors = []);
+      !b.neighbors && (b.neighbors = []);
+      a.neighbors.push(b);
+      b.neighbors.push(a);
 
-    fgRef.current.cameraPosition(
-      { x: node.x * distRatio, y: node.y * distRatio, z: node.z * distRatio }, // new position
-      node, // lookAt ({ x, y, z })
-      3000  // ms transition duration
-    );
-  }, [fgRef]);
+      !a.links && (a.links = []);
+      !b.links && (b.links = []);
+      a.links.push(link);
+      b.links.push(link);
+    });
 
+    return gData;
+  }, []);
+  const [highlightNodes, setHighlightNodes] = useState(new Set());
+      const [highlightLinks, setHighlightLinks] = useState(new Set());
+      const [hoverNode, setHoverNode] = useState(null);
+
+      const updateHighlight = () => {
+        setHighlightNodes(highlightNodes);
+        setHighlightLinks(highlightLinks);
+      };
+
+      const handleNodeHover = node => {
+        highlightNodes.clear();
+        highlightLinks.clear();
+        if (node) {
+          highlightNodes.add(node);
+          node.neighbors.forEach(neighbor => highlightNodes.add(neighbor));
+          node.links.forEach(link => highlightLinks.add(link));
+        }
+
+        setHoverNode(node || null);
+        updateHighlight();
+      };
+
+      const handleLinkHover = link => {
+        highlightNodes.clear();
+        highlightLinks.clear();
+
+        if (link) {
+          highlightLinks.add(link);
+          highlightNodes.add(link.source);
+          highlightNodes.add(link.target);
+        }
+
+        updateHighlight();
+      };
+
+      const paintRing = useCallback((node, ctx) => {
+        // add ring just for highlighted nodes
+        ctx.beginPath();
+        ctx.arc(node.x, node.y, NODE_R * 1.4, 0, 2 * Math.PI, false);
+        ctx.fillStyle = node === hoverNode ? 'red' : 'orange';
+        ctx.fill();
+      }, [hoverNode]);
   return (
-    <ForceGraph3D
-      ref={fgRef}
-      graphData={createNodesAndEdges()}
+    <ForceGraph2D
+      graphData={data}
       nodeLabel="name"
-      nodeAutoColorBy="group"
-      onNodeClick={handleClick}
+      nodeRelSize={NODE_R}
+      linkWidth={link => highlightLinks.has(link) ? 5 : 1}
+      linkDirectionalParticles={4}
+      linkDirectionalParticleWidth={link => highlightLinks.has(link) ? 4 : 0}
+      nodeCanvasObjectMode={node => highlightNodes.has(node) ? 'before' : undefined}
+      nodeCanvasObject={paintRing}
+      onNodeHover={handleNodeHover}
+      onLinkHover={handleLinkHover}
     />
   );
 }
